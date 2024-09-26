b29d02b92edc08318e7052cfc54eaec2f4db23d5
Shoib khan <shoibk897@gmail.com>
2024-09-26 20:48:55 +0530

    Added an intro animation

--- App/index.js
+++ App/index.js
@@ -1,3 +1,33 @@
+let intro = document.querySelector('.intro-animation');
+let logo = document.querySelector('.intro-box');
+let logospan = document.querySelectorAll('.intro-logo');
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    setTimeout(() => {
+
+        logospan.forEach((span, idx) => {
+            setTimeout(() => {
+                span.classList.add('active');
+            }, (idx + 1) * 400)
+        });
+
+        setTimeout(()=>{
+            logospan.forEach((span,idx)=>{
+
+                setTimeout(()=>{
+                    span.classList.remove('active');
+                    span.classList.add('fade');
+                },(idx + 1) * 50)
+            })
+        },2000);
+
+        setTimeout(()=>{
+            intro.style.top ='-100vh';
+        },2300);
+    })
+});
+
 function render() {
 
     function getAllNotes() {
